c33e363ecd46a4038d96870209fdf601f20db186
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2022-01-28 13:02:08 +0200

    initial commit

---
+++ src/components/ComboBox.js
@@ -0,0 +1,20 @@
+//Library imports
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
+
+export const ComboBox = (props) => {
+  return (
+    <div>
+      <Autocomplete
+        multiple
+        id={props.id}
+        style={{ width: props.width, display: "block" }}
+        options={props.options}
+        renderInput={(params) => (
+          <TextField {...params} label={props.label} variant="outlined" />
+        )}
+      />
+    </div>
+  );
+};
